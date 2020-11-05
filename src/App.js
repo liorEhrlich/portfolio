0eddd72c0f6571a5551697dbf2027c43a3dd7676
Lior Ehrlich <liorehrlich@gmail.com>
2020-11-05 19:29:02 +0200

    install emotion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import styled from '@emotion/styled'
+import { ThemeProvider } from 'emotion-theming'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>    
+      <Wrapper>
+        <Name>I'm Leor Ehrlich</Name>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
+
+const Wrapper = styled.div`
+  background-color: ${props => props.theme.colors.lightOrange};
+`
+
+const Name = styled.h1`
+  color: ${props => props.theme.colors.pink};
+`
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    pink: '#D9C1D0',
+    orangeBrown: '#f29f05',
+    lightOrange: '#F28705',
+    persimmon: '#F25C05',
+    strawberry: '#F20505'
+  }
+}
+
+export default theme
