c05a3e1657b0d328c64c0ecd0f1bd453114ef173
Lior Ehrlich <liorehrlich@gmail.com>
2020-11-06 10:30:05 +0200

    add canvas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import styled from '@emotion/styled'
 import { ThemeProvider } from 'emotion-theming'
+import Canvas from './canvas'
 
-import './App.css';
+import theme from './theme'
+
 
 function App() {
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.fill()
+  }
+
   return (
     <ThemeProvider theme={theme}>    
       <Wrapper>
         <Name>I'm Leor Ehrlich</Name>
+        <Canvas draw={draw}/>
       </Wrapper>
     </ThemeProvider>
   );
